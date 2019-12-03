e0dce20f044fbbdb1abd56baf13e069576912f8d
jocelyn <961951661@qq.com>
2019-12-03 17:56:00 +0800

    chore:

---
+++ src/views/Index/Index.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="index">
+    <!-- <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    <GlobalFooter />
+  </div>
+</template>
+
+<script lang="ts">
+import { Component, Prop, Vue } from 'vue-property-decorator';
+import HelloWorld from '@/components/HelloWorld.vue';
+import GlobalFooter from '@/components/GlobalFooter/GlobalFooter.vue';
+
+@Component({
+  components: {
+    HelloWorld,
+    GlobalFooter,
+  },
+})
+export default class Index extends Vue {
+  // @Prop() private active!: string;
+}
+</script>
