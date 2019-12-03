189977889338f089b9cad01364cf419c65e57da9
jocelyn <961951661@qq.com>
2019-12-03 16:09:12 +0800

    chore: px2rem  vant

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,9 @@ import Vue from 'vue';
 import App from './App.vue';
 import router from './router';
 import store from './store';
+import setRem from './utils/setRem';
+
+setRem();
 
 Vue.config.productionTip = false;
 
